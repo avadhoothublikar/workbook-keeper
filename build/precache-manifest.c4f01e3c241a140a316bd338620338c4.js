4a4e2b85e930880e86d91e5f2dbc8f29cc7fc019
avadhoothublikar <107243417+avadhoothublikar@users.noreply.github.com>
2022-11-02 16:26:41 +0530

    Add files via upload

---
+++ build/precache-manifest.c4f01e3c241a140a316bd338620338c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a9109ab2bc9171ffbdd1819d4b2976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56ecc0cc98977a154b1a",
+    "url": "/static/js/2.066f746b.chunk.js"
+  },
+  {
+    "revision": "9d2b49cfcfc8c39cf22a",
+    "url": "/static/js/main.5af6815b.chunk.js"
+  },
+  {
+    "revision": "84832877ed76605d723e",
+    "url": "/static/js/runtime-main.aa743321.js"
+  }
+]);
